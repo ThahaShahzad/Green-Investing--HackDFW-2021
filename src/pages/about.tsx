a0b1315b7b6613f550cd5c40555920f86f3da178
ThahaShahzad <61521512+ThahaShahzad@users.noreply.github.com>
2021-10-02 21:49:03 -0500

    Build error fix Quote

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 import LandingLayout from 'components/landing/layout'
-import { Heading, Container, Text } from '@chakra-ui/react'
+import { Heading, Container, Text, Link } from '@chakra-ui/react'
 
 const About = () => {
   return (
@@ -18,9 +18,16 @@ const About = () => {
         <Heading py='4'>About</Heading>
         <Text>What is ESG?</Text>
         <Text>
-          ESG is a metric used to measure how "green" a company is based on their environmental, social, and corporate
-          governance practices. Each category is given a score and the final ESG score is their sum. Learn more:
-          https://en.wikipedia.org/wiki/Environmental,_social_and_corporate_governance
+          ESG is a metric used to measure how &quot;green&quot; a company is based on their environmental, social, and
+          corporate governance practices. Each category is given a score and the final ESG score is their sum.
+          <Link
+            href='https://en.wikipedia.org/wiki/Environmental,_social_and_corporate_governance'
+            target='_blank'
+            ml='1'
+            textDecoration='underline'
+            rel='noopener'>
+            Learn more{' '}
+          </Link>
         </Text>
       </Container>
     </>
